2074aa20617dd641234fe54a6be1901dca81957f
TanmayGupta2248 <Gtanmay295@gmail.com>
2024-05-31 15:12:51 +0530

    feat(app): added landing page

---
+++ app/src/main/java/com/example/greenkart1/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.greenkart1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button3 = findViewById(R.id.button3); // Use the correct ID for your login button
+        Button button4= findViewById(R.id.button4); // Use the correct ID for your register button
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Register_Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
